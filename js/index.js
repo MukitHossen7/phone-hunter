dacb43934b95853bd7268ca0c2a901cbf639a69d
MukitHossen7 <hossenmukit7@gmail.com>
2024-10-04 21:32:44 +0600

    display data show

--- js/index.js
+++ js/index.js
@@ -1,24 +1,56 @@
-const loadAllData = async () => {
+const loadAllData = async (isClick, searchText) => {
   document.getElementById("loadingContainer").classList.add("hidden");
 
   const res = await fetch(
-    `https://openapi.programming-hero.com/api/phones?search=iphone`
+    `https://openapi.programming-hero.com/api/phones?search=${
+      searchText ? searchText : "iphone"
+    }`
   );
   const phoneData = await res.json();
-  displayLoadPhone(phoneData.data);
+  isClick
+    ? displayLoadPhone(phoneData.data)
+    : displayLoadPhone(phoneData.data.slice(0, 6));
 };
+
+// show-all-button-function
+const showAllBtnFunction = () => {
+  loadAllData(true);
+};
+// show-all-button-function
 
 // display-load-phones
 const displayLoadPhone = (phones) => {
-  console.log(phones);
+  const cardContainer = document.getElementById("card-container");
+  phones.forEach((phone) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="card border border-slate-200 p-9">
+  <figure class="px-10 py-12 bg-[#0D6EFD0D] rounded-xl">
+    <img
+      src="${phone.image}"
+      alt="phones"
+      class="rounded-xl" />
+  </figure>
+  <div class="card-body items-center text-center">
+    <h2 class="font-bold text-2xl text-dark2">${phone.phone_name}</h2>
+    <p class="text-dark3 font-medium">There are many variations of passages of available, but the majority have suffered</p>
+    <div class="card-actions">
+      <button class="btn btn-primary text-white">Show Details</button>
+    </div>
+  </div>
+</div>
+    `;
+    cardContainer.append(div);
+  });
 };
 // display-all-phones
 
 // show-loading-function
 const loadingDisplay = () => {
   document.getElementById("loadingContainer").classList.remove("hidden");
+  const searchText = document.getElementById("inputValue").value;
   setTimeout(() => {
-    loadAllData();
+    loadAllData(false, `${searchText}`);
   }, 3000);
 };
 // show-loading-function
